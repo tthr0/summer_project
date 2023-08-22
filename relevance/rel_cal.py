24d5eb7849541d3499c2b11810d91417765800cc
河辺辰大 <kawabetatsuhiro@C8H11NO2.local>
2023-08-22 23:28:51 +0900

    1

---
+++ relevance/rel_cal.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+website_url = "https://news.yahoo.co.jp/articles/334aa147c0209ac515a977fffb52b0ed56fc5132"  # 対象のウェブサイトのURL
+values_to_check = ["野球", "甲子園", "サッカー"]  # 関連性を調べる値のリスト
+
+# ウェブサイトへアクセスしてページのHTMLを取得
+response = requests.get(website_url)
+html_content = response.text
+soup = BeautifulSoup(html_content, 'html.parser')
+
+related_weight = 0
+
+
+for value in values_to_check:
+    related = False
+
+    for paragraph in soup.find_all('p'):
+        if value in paragraph.get_text():
+            related = True
+            related_weight += 1
+            break
+    
+    if related:
+        print(f"値 '{value}' は関連性があります。")
+    else:
+        print(f"値 '{value}' は関連性がありません。")
+
+print("関連度:",related_weight)
